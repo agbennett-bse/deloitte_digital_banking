91680caf9efeb3472b198d4f175e619aa4d0f950
agbennett-bse <andrew.bennett@bse.eu>
2024-04-26 09:15:37 +0200

    initial commit

---
+++ way_back_scraper/reqs.py
@@ -0,0 +1,10 @@
+import requests
+from fake_useragent import UserAgent
+from fp.fp import FreeProxy
+import time
+import plotly.express as px
+from googletrans import Translator
+import os
+import pandas as pd
+from tqdm import tqdm
+tqdm.pandas()
